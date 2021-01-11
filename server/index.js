20f22cff12a1dd51dd2b00c41d2f091c3eacff8a
jacoborbach <jacoborbach96@gmail.com>
2021-01-11 16:18:51 -0500

    finished step 5

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const massive = require('massive'),
     express = require('express'),
     { SERVER_PORT, CONNECTION_STRING } = process.env,
+    productsCtrl = require('./controllers/products_controller'),
     app = express();
 
 app.use(express.json());
@@ -13,6 +14,13 @@ massive({
     app.set('db', db);
     console.log('db connected')
 }).catch(err => console.log(err));
+
+app.get('/api/products', productsCtrl.getAll);
+app.get('/api/products/:id', productsCtrl.getOne);
+app.put('/api/products/:id', productsCtrl.update);
+app.post('/api/products', productsCtrl.create);
+app.delete('/api/products/:id', productsCtrl.delete);
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`You are listening on server ${SERVER_PORT}`);
