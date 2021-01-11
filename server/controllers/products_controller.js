3b5e180322c51abb1be1f47844daa035251ac5cd
jacoborbach <jacoborbach96@gmail.com>
2021-01-11 16:24:07 -0500

    finished step 6

--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -1,35 +1,37 @@
 module.exports = {
-    create: (req, res) => {
+    create: (req, res, next, next) => {
+        const { name, description, price, image_url } = req.body;
         const db = req.app.get('db');
 
-        db.create_product()
+        db.create_product([name, description, price, image_url])
             .then(product => res.status(200).send(product))
             .catch(err => res.status(500).send(err))
     },
-    getOne: (req, res) => {
+    getOne: (req, res, next) => {
+        const { id } = req.params;
         const db = req.app.get('db');
 
-        db.read_product()
+        db.read_product(id)
             .then(product => res.status(200).send(product))
             .catch(err => res.status(500).send(err))
     },
-    getAll: (req, res) => {
+    getAll: (req, res, next) => {
         const db = req.app.get('db')
 
         db.read_products()
             .then(product => res.status(200).send(product))
             .catch(err => res.status(500).send(err))
     },
-    update: (req, res) => {
+    update: (req, res, next) => {
         const { id } = req.params,
-            { description } = req.body;
+            { description } = req.query;
         const db = req.app.get('db')
 
         db.update_product(description, id)
             .then(product => res.status(200).send(product))
             .catch(err => res.status(500).send(err));
     },
-    delete: (req, res) => {
+    delete: (req, res, next) => {
         const { id } = req.params;
 
         const db = req.app.get('db')
